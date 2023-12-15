fc9209216bf2b1ba27b9e7f86a1be2af377a2e74
Kaz <ccc5211@gmail.com>
2023-12-15 15:13:06 +0800

    feat: add login.aspx and login logic

---
+++ GitDemo/login.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GitDemo
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void ButtonLogIn_Click(object sender, EventArgs e)
+        {
+            if (TextBoxAccount.Text == "Leader" && TextBoxPassowrd.Text == "123456")
+            {
+                Response.Redirect("https://www.google.com");
+            }
+            else
+            {
+                LabelWarning.Text = "帳號或密碼錯誤！";
+            }
+        }
+    }
+}
---
+++ GitDemo/login.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <自動產生>
+//     這段程式碼是由工具產生的。
+//
+//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
+//     程式碼，則會遺失變更。
+// </自動產生>
+//------------------------------------------------------------------------------
+
+namespace GitDemo
+{
+
+
+    public partial class Login
+    {
+
+        /// <summary>
+        /// form1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// LabelAccount 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelAccount;
+
+        /// <summary>
+        /// TextBoxAccount 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxAccount;
+
+        /// <summary>
+        /// LabelPassword 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelPassword;
+
+        /// <summary>
+        /// TextBoxPassowrd 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxPassowrd;
+
+        /// <summary>
+        /// ButtonLogIn 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonLogIn;
+
+        /// <summary>
+        /// LabelWarning 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelWarning;
+    }
+}
